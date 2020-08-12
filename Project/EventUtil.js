3c8a1f7e54ae7846503afa79b4f146770639929b
Wah-Wah-Wynn-Shwe <kaunghtetsan.ucsh@gmail.com>
2020-08-11 19:38:35 -0700

    first commit

---
+++ Project/EventUtil.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<head>
+<title></title>
+</head>
+<body>
+<script type="text/javascript">
+var EventUtil={
+addHandler:function(element,type,handler){
+if (element.addEventListener){
+element.addEventListener(type,handler,false);
+} else if  (element.attachEvent){
+element.attachEvent("on"+type,handler);
+} else {
+element["on"+type]=handler;
+}
+},
+
+
+removeHandler:function(element,type,handler){
+if (element.removeEventListener){
+element.removeEventListener(type,handler,false);
+} else if  (element.detachEvent){
+element.detachEvent("on"+type,handler);
+} else {
+element["on"+type]=null;
+}
+}
+};
+</script>
+</body>
+</html>
